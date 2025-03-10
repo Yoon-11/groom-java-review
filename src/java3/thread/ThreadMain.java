25a51bc5b341c47144dc729f40b0da2d7412675c
yoon <jiys5799@gmail.com>
2025-03-10 15:14:48 +0900

    Add thread package

---
+++ src/java3/thread/ThreadMain.java
@@ -0,0 +1,40 @@
+package java3.thread;
+
+public class ThreadMain {
+    public static void main(String[] args) {
+        Thread thread1 = new Task1();
+        Thread thread2 = new Task2();
+
+        thread1.start();
+        thread2.start();
+    }
+
+}
+
+class Task1 extends Thread {
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Task1 실행 중 " + i);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class Task2 extends Thread {
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Task2 실행 중 " + i);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+
